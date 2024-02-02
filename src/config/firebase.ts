0bdcd6100b912924ce749c9eae0772d6d1abb8fa
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2024-02-02 20:29:32 -0300

    instala Firebase, Storage e inicializa config firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth/react-native';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBZDQFfewyN6Lb_S28GFDQ3rHqc1sRWe7s',
+  authDomain: 'alura-firebase-rn.firebaseapp.com',
+  projectId: 'alura-firebase-rn',
+  storageBucket: 'alura-firebase-rn.appspot.com',
+  messagingSenderId: '718914062166',
+  appId: '1:718914062166:web:80b1873a772602157f38f7',
+  measurementId: 'G-624V54KQFK',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { auth, createUserWithEmailAndPassword };
